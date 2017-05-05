0897d6b20fc55ce95f46ecbacd57b578ba08cb53
Daniel St. John <overmandan@comcast.net>
2017-05-05 00:35:14 -0600

    first commit zombe flappy udem tutorial

---
+++ Assets/Scripts/LinearConstantSpeedMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearConstantSpeedMovement : MonoBehaviour {
+
+	public Transform leftLimit;
+
+	public Transform resetPossition;
+
+ 	public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.localPosition = new Vector3(transform.localPosition.x +Time.deltaTime*speed, transform.localPosition.y, transform.localPosition.z);
+
+		if ( transform.localPosition.x > leftLimit.localPosition.x){
+			transform.localPosition = new Vector3(resetPossition.localPosition.x , transform.localPosition.y, transform.localPosition.z);
+		}
+	}
+}
---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour {
+
+	public bool useLocal = true;
+
+	public Transform[] waypoints;
+
+	public bool isWaitBetweenWaypoints;
+
+	public float waitTimeBetweenWaypoint = .5f;
+
+	public bool isRepeating = true;
+
+	public float speed = 1f;
+
+	public float threshhold = .1f;
+
+	void Start () {
+		StartCoroutine ( Move(waypoints[0]) ); 
+	}
+
+	int currentWaypointIndex = 0;
+
+	IEnumerator Move( Transform currentWaypoint){
+
+		float dist = 0;
+
+		if ( useLocal) {
+		  dist =  Vector3.Distance(transform.position, currentWaypoint.position);
+		}else {
+		 dist =  Vector3.Distance(transform.localPosition, currentWaypoint.localPosition);
+		}
+ 
+
+		while ( dist > threshhold)
+		{
+			float step = speed * Time.deltaTime;
+
+			if ( useLocal) {
+				transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, step);
+			}else {
+				transform.position = Vector3.MoveTowards(transform.localPosition, currentWaypoint.localPosition, step);
+			}
+		 
+
+			if ( useLocal) {
+				dist =  Vector3.Distance(transform.position, currentWaypoint.position);
+			}else {
+				dist =  Vector3.Distance(transform.localPosition, currentWaypoint.localPosition);
+			}
+
+			yield return null;
+		}
+
+		yield return new WaitForSeconds (waitTimeBetweenWaypoint);
+
+		currentWaypointIndex++;
+
+		if ( currentWaypointIndex < waypoints.Length ){
+			StartCoroutine (Move(waypoints[currentWaypointIndex]));
+		} else {
+
+			if ( isRepeating ){
+				currentWaypointIndex = 0;
+				StartCoroutine (Move(waypoints[currentWaypointIndex]));
+			}
+
+		}
+
+	}
+}
